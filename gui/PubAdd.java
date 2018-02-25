b9a32b4bc62290140c47bf4cb5374a5f8f5a061d
Manuela Hopp <manuelahopp@gmx.net>
2018-02-25 18:55:23 +0100

    gui form for entering incollection

--- gui/PubAdd.java
+++ gui/PubAdd.java
@@ -26,14 +26,14 @@ public class PubAdd extends JPanel implements ActionListener {
 	private JComboBox<String> pubClassList;
 	
 	PubAdd(){
-		Dimension dim = new Dimension(500,500);
+		Dimension dim = new Dimension(600,600);
 		setPreferredSize(dim);
 		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 		
 		JPanel classListPanel = new JPanel();
 		pubClasses = new String[] {"article", "book", "booklet", "inbook", "incollection", "inproceedings", "manual", "mastersthesis", "misc", "phdthesis", "proceedings", "techreport", "unpublished"};
 		pubClassList = new JComboBox<String>(pubClasses);
-		pubClassList.setPreferredSize(new Dimension(100,25));
+		pubClassList.setPreferredSize(new Dimension(150,25));
 		pubClassList.setSelectedIndex(0);
 		pubClassList.addActionListener(this);
 		classListPanel.add(pubClassList);
@@ -61,6 +61,7 @@ public class PubAdd extends JPanel implements ActionListener {
 		if (pubClassSel.equals("book")) showBook();
 		if (pubClassSel.equals("booklet")) showBooklet();
 		if (pubClassSel.equals("inbook")) showInbook();
+		if (pubClassSel.equals("incollection")) showIncollection();
 	}
 	
 	private void showArticle() {
@@ -432,7 +433,6 @@ public class PubAdd extends JPanel implements ActionListener {
 		c.weightx = 1; c.gridx = 1; c.gridy = 4;
 		panel.add(yearF, c);
 		
-		//edition, month, note, key
 		// now the optional fields
 		c.insets = new Insets(10,0,0,0);
 		
@@ -506,10 +506,159 @@ public class PubAdd extends JPanel implements ActionListener {
 		revalidate();
 	}
 	
-	boolean containsComponent(JPanel p, Component c) {
-		Component[] components = p.getComponents();
-		for (Component component : components) {
-			if (c == component) {
+	private void showIncollection() {
+		panel.removeAll();
+		panel.repaint();
+		Dimension dim = new Dimension(200,25);
+		
+		JLabel titleL = new JLabel("Title: ");
+		JTextField titleF = new JTextField();
+		titleF.setPreferredSize(dim);
+	    c.weightx = 0; c.gridx = 0; c.gridy = 0; c.insets = new Insets(0,0,0,0);
+		panel.add(titleL, c);
+		c.weightx = 1; c.gridx = 1; c.gridy = 0;
+		panel.add(titleF, c);
+		
+		JLabel booktitleL = new JLabel("Title: ");
+	    c.weightx = 0; c.gridx = 0; c.gridy = 1;
+		panel.add(booktitleL, c);
+		JTextField booktitleF = new JTextField();
+		booktitleF.setPreferredSize(dim);
+		c.weightx = 1; c.gridx = 1; c.gridy = 1;
+		panel.add(booktitleF, c);
+		
+		JLabel authorL = new JLabel("Author(s): ");
+		JTextField authorF = new JTextField();
+		authorF.setPreferredSize(dim);
+		c.weightx = 0; c.gridx = 0; c.gridy = 2;
+		panel.add(authorL, c);
+		c.weightx = 1; c.gridx = 1; c.gridy = 2;
+		panel.add(authorF, c);
+		
+		JLabel publL = new JLabel("Publisher: ");
+		c.weightx = 0; c.gridx = 0; c.gridy = 3;
+		panel.add(publL, c);
+		JTextField publF = new JTextField();
+		publF.setPreferredSize(dim);
+		c.weightx = 1; c.gridx = 1; c.gridy = 3;
+		panel.add(publF, c);
+		
+		JLabel yearL = new JLabel("Year: ");
+		c.weightx = 0; c.gridx = 0; c.gridy = 4;
+		panel.add(yearL, c);
+		JTextField yearF = new JTextField(Year.now().toString());
+		yearF.setPreferredSize(dim);
+		c.weightx = 1; c.gridx = 1; c.gridy = 4;
+		panel.add(yearF, c);
+		
+		// now the optional fields
+		c.insets = new Insets(10,0,0,0);
+		
+		JLabel edL = new JLabel("Editor: ");
+		c.weightx = 0; c.gridx = 0; c.gridy = 5;
+		panel.add(edL, c);
+		JTextField edF = new JTextField();
+		edF.setPreferredSize(dim);
+		c.weightx = 1; c.gridx = 1; c.gridy = 5;
+		panel.add(edF, c);
+		
+		String[] volnum = {"Volume", "Number"};
+		JComboBox<String> vnBox = new JComboBox<String>(volnum);
+		vnBox.setPreferredSize(new Dimension(90, 23));
+		vnBox.setSelectedIndex(0);
+	    c.weightx = 0; c.gridx = 0; c.gridy = 6; c.insets = new Insets(0,0,0,5);
+		panel.add(vnBox, c);
+		JTextField volNumF = new JTextField();
+		volNumF.setPreferredSize(dim);
+		c.weightx = 1; c.gridx = 1; c.gridy = 6; c.insets = new Insets(0,0,0,0);
+		panel.add(volNumF, c);
+		
+		JLabel seriesL = new JLabel("Series: ");
+		c.weightx = 0; c.gridx = 0; c.gridy = 7;
+		panel.add(seriesL, c);
+		JTextField seriesF = new JTextField();
+		seriesF.setPreferredSize(dim);
+		c.weightx = 1; c.gridx = 1; c.gridy = 7;
+		panel.add(seriesF, c);
+		
+		JLabel typeL = new JLabel("Type: ");
+		c.weightx = 0; c.gridx = 0; c.gridy = 8;
+		panel.add(typeL, c);
+		JTextField typeF = new JTextField();
+		typeF.setPreferredSize(dim);
+		c.weightx = 1; c.gridx = 1; c.gridy = 8;
+		panel.add(typeF, c);
+		
+		JLabel chapL = new JLabel("Chapter: ");
+		c.weightx = 0; c.gridx = 0; c.gridy = 9;
+		panel.add(chapL, c);
+		JTextField chapF = new JTextField();
+		chapF.setPreferredSize(dim);
+		c.weightx = 1; c.gridx = 1; c.gridy = 9;
+		panel.add(chapF, c);
+		
+		JLabel pagesL = new JLabel("Pages: ");
+		c.weightx = 0; c.gridx = 0; c.gridy = 10;
+		panel.add(pagesL, c);
+		JPanel pagesPanel = new JPanel();
+		JTextField pagesF1 = new JTextField();
+		pagesF1.setPreferredSize(new Dimension(50,25));
+		pagesPanel.add(pagesF1);
+		JLabel minus = new JLabel(" - ");
+		pagesPanel.add(minus);
+		JTextField pagesF2 = new JTextField();
+		pagesF2.setPreferredSize(new Dimension(50,25));
+		pagesPanel.add(pagesF2);
+		c.weightx = 1; c.gridx = 1; c.gridy = 10;
+		panel.add(pagesPanel, c);
+		
+		JLabel addressL = new JLabel("Address: ");
+		c.weightx = 0; c.gridx = 0; c.gridy = 11;
+		panel.add(addressL, c);
+		JTextField addressF = new JTextField();
+		addressF.setPreferredSize(dim);
+		c.weightx = 1; c.gridx = 1; c.gridy = 11;
+		panel.add(addressF, c);
+		
+		JLabel editionL = new JLabel("Edition: ");
+		c.weightx = 0; c.gridx = 0; c.gridy = 12;
+		panel.add(editionL, c);
+		JTextField editionF = new JTextField();
+		editionF.setPreferredSize(dim);
+		c.weightx = 1; c.gridx = 1; c.gridy = 12;
+		panel.add(editionF, c);
+		
+		JLabel monthL = new JLabel("Month: ");
+		c.weightx = 0; c.gridx = 0; c.gridy = 13;
+		panel.add(monthL, c);
+		JTextField monthF = new JTextField();
+		monthF.setPreferredSize(dim);
+		c.weightx = 1; c.gridx = 1; c.gridy = 13;
+		panel.add(monthF, c);
+		
+		JLabel keyL = new JLabel("Key: ");
+		c.weightx = 0; c.gridx = 0; c.gridy = 14;
+		panel.add(keyL, c);
+		JTextField keyF = new JTextField();
+		keyF.setPreferredSize(dim);
+		c.weightx = 1; c.gridx = 1; c.gridy = 14;
+		panel.add(keyF, c);
+		
+		JLabel noteL = new JLabel("Note: ");
+		c.weightx = 0; c.gridx = 0; c.gridy = 15; c.insets = new Insets(5,0,0,0);
+		panel.add(noteL, c);
+		JTextArea noteF = new JTextArea();
+		noteF.setPreferredSize(new Dimension(200,50));
+		c.weightx = 1; c.gridx = 1; c.gridy = 15;
+		panel.add(noteF, c);
+		
+		revalidate();
+	}
+	
+	private boolean containsComponent(JPanel p, Component c) {
+		Component[] comps = p.getComponents();
+		for (Component comp : comps) {
+			if (c == comp) {
 				return true;
 			}
 		}
